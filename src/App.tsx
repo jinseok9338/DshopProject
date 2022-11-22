8f9f6b40d4ff14fdf0d109568c42c4e1993d36cb
jinseok9338 <jinseok9338@gmail.com>
2022-11-22 17:45:10 +0900

    making Vite project

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <input type="text" placeholder="Search" />
+      <button>Search</button>
+    </div>
+  );
+}
+
+export default App;
